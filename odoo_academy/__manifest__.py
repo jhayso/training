597afb6dfd2a6c0275bbbce81d1b4b17ab1d29f1
JHAYSO <jhalvarez@grupojhayso.com>
2022-11-10 16:37:45 +0000

    [ADD] Basic Module

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+{
+
+    'name': 'Odoo Academy',
+
+    'summary': """Academy app to manage Training""",
+
+    'description': """
+    Academy Modulo to manage Training:
+    - Courses
+    - Sessions
+    - Attendees
+    """,
+
+    'author': 'JHAYSO',
+
+    'website': 'https://www.grupojhayso.com',
+
+    'category': 'Training',
+    'version': '1.0',
+    
+    'depends': ['base'],
+    
+    'data':[
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
